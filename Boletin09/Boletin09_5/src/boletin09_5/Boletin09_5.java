677c9cb48a574804331f4bfc94297810db1dbe9f
PedroPineiro <ppineiroordax@danielcastelao.org>
2023-11-15 14:24:57 +0100

    Boletin09_5

---
+++ Boletin09/Boletin09_5/src/boletin09_5/Boletin09_5.java
@@ -0,0 +1,12 @@
+package boletin09_5;
+
+public class Boletin09_5 {
+
+    public static void main(String[] args) {
+
+        SoldoTraballador st = new SoldoTraballador();
+        st.calcularSoldo();
+    }
+
+}
+    
