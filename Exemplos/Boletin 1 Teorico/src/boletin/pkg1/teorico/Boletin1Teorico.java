39789baad02ae25adce27c4b2938481ce47006a5
PedroPineiro <ppineiroordax@danielcastelao.org>
2023-11-15 14:33:34 +0100

    BoletinTeorico01

---
+++ Exemplos/Boletin 1 Teorico/src/boletin/pkg1/teorico/Boletin1Teorico.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package boletin.pkg1.teorico;
+
+/**
+ *
+ * @author dam1
+ */
+
+   class Boletin1Teorico {
+   public static void main(String[] args){
+   int i = 3;
+   i++;
+   System.out.println(i);	// 4
+   ++i;
+   System.out.println(i);	// 5
+   System.out.println(++i);	// 6
+   System.out.println(i++);	// 6
+    System.out.println(i);	// 7
+    }
+    
+    
+    
+}
