c6dbf8ad779f4139a6a9c180121689e3d6ee5945
PedroPineiro <ppineiroordax@danielcastelao.org>
2023-11-15 13:26:15 +0100

    Boletin07_3

---
+++ Boletin07/Boletin07_3/src/boletin07_3/Clase.java
@@ -0,0 +1,19 @@
+package boletin07_3;
+
+public class Clase {
+    public void verSigno(int num){
+        
+        if(num>0){
+            System.out.println("+");
+        }
+        else if(num==0){
+                System.out.println("0");
+        }
+            else{
+                System.out.println("-");
+            }
+
+            }
+        }
+        
+    
