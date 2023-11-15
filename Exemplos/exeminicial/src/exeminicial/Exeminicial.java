8aea5557f7430559a8d2f1c908918e782cb282c9
PedroPineiro <ppineiroordax@danielcastelao.org>
2023-11-15 14:35:30 +0100

    ExInicial01

---
+++ Exemplos/exeminicial/src/exeminicial/Exeminicial.java
@@ -0,0 +1,14 @@
+
+package exeminicial;
+
+public class Exeminicial {
+
+    public static void main(String[] args) {
+       float base= 5, altura= 3, area; 
+       area=base*altura;
+       System.out.println("area do rectangulo = "+area);
+       
+    }
+    
+}
+ 
