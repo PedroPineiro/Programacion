762d7771b487623fb5ef78103626d71d5ac38da6
PedroPineiro <ppineiroordax@danielcastelao.org>
2023-11-17 14:01:42 +0100

    ExNumAleatorios

---
+++ Exemplos/EjsEjemplo/src/ejsejemplo/NumAleatoriosEntre.java
@@ -0,0 +1,10 @@
+package ejsejemplo;
+
+public class NumAleatoriosEntre {
+
+    public static void main(String[] args) {
+        int valorEntero = (int) (Math.floor(Math.random()*(40-1+1)+1));
+        System.out.println(valorEntero);
+    }
+    
+}
