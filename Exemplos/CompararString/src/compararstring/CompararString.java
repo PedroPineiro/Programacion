786f1af6ed1b3291f629bf0611dd9c0a9354ef8d
PedroPineiro <ppineiroordax@danielcastelao.org>
2023-11-17 14:02:42 +0100

    ExCompararString

---
+++ Exemplos/CompararString/src/compararstring/CompararString.java
@@ -0,0 +1,11 @@
+package compararstring;
+
+public class CompararString {
+
+    public static void main(String[] args) {
+        String cadena1 = new String("Hola");
+        String cadena2 = new String("Hola");
+        if (cadena1 == cadena2)
+    }
+    
+}
