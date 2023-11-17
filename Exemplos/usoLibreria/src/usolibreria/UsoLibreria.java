36abbb5c119394e7db812954d4f97058883d320d
PedroPineiro <ppineiroordax@danielcastelao.org>
2023-11-17 13:57:16 +0100

    UsoLibreria

---
+++ Exemplos/usoLibreria/src/usolibreria/UsoLibreria.java
@@ -0,0 +1,14 @@
+package usolibreria;
+
+public class UsoLibreria {
+
+    public static void main(String[] args) {
+        //se puede hacer de las dos formas
+       // int dato=lerDatos.lerEnteiro("teclea dato");
+       // System.out.println(dato);
+        
+       //o
+        System.out.println("dato="+lerDatos.lerEnteiro("dato:"));
+    }
+    
+}
