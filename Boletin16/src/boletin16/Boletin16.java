9bf41b4f9bedc458620bf0980ccd84e859dd119a
Pedro Pineiro <pedropineiro013@gmail.com>
2023-12-06 00:42:07 +0100

    Boletin16

---
+++ Boletin16/src/boletin16/Boletin16.java
@@ -0,0 +1,13 @@
+
+
+package boletin16;
+
+
+public class Boletin16 {
+    public static void main(String[] args) {
+
+        Publicacion libr=new Libro("Non",1,"La ligera levedad del ser",2015);
+        System.out.println(libr.toString());
+
+    }
+}
---
+++ Boletin16/src/boletin16/Revista.java
@@ -0,0 +1,32 @@
+package boletin16;
+
+public class Revista extends Publicacion {
+
+    private int numero;
+
+    public Revista() {
+    }
+
+    public Revista(int numero) {
+        this.numero = numero;
+    }
+
+    public Revista(int numero, int codigo, String titulo, int ano) {
+        super(codigo, titulo, ano);
+        this.numero = numero;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "revista{" + "numero=" + numero + '}';
+    }
+
+}
