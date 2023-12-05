152e56b294f89cf4b073744b87f1459476ff6dfa
Pedro Pineiro <pedropineiro013@gmail.com>
2023-12-06 00:39:45 +0100

    Boletin12

---
+++ Boletin12/src/boletin12/Boletin12.java
@@ -0,0 +1,11 @@
+package boletin12;
+
+public class Boletin12 {
+
+    public static void main(String[] args) {
+        do {
+            Garaje gar = new Garaje();
+            gar.sitio();
+        } while (true);
+    }
+}
